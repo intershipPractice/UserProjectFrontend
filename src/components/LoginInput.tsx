1f5bf85d3c6e816355379311b3bfce0c5b5f4df8
sykang519 <sykang@swrd.cc>
2024-09-13 12:11:56 +0900

    feat: write sign in, sing up logic

--- src/components/LoginInput.tsx
+++ src/components/LoginInput.tsx
@@ -1,33 +1,61 @@
 import styles from './LoginInput.module.css';
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-
+import {useState} from 'react';
 
 
 function LoginInput() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e:React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/v1/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify({ email, password }),
+      });
+    
+      if(!response.ok){
+        alert("로그인 실패");
+      }
+    }catch (error){
+      alert("로그인 실패");
+    }
+};
+
   return (
     <div className={styles.container}>
       <h2 className={styles.text}>로그인</h2>
-    <form className={styles.form}>
-      <TextField 
-        className={styles.inputBox}
-        id="demo-helper-text-misaligned"
-        label="email"
-      />
-      <TextField 
-        className={styles.inputBox}
-        id="demo-helper-text-misaligned-no-helper" 
-        label="password" 
-        type="password"
-      />
-      <Stack spacing={2} direction="row">
-        <Button className={styles.button} variant="contained">로그인 하기</Button>
-      </Stack>
-    </form>
-    <p className={styles.text}><a href="http://localhost:3000/join">회원이 아니신가요?</a></p>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <TextField 
+          className={styles.inputBox}
+          id="demo-helper-text-misaligned"
+          label="이메일"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        /><br/>
+        <TextField 
+          className={styles.inputBox}
+          id="demo-helper-text-misaligned-no-helper" 
+          label="비밀번호" 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        /><br/>
+        <Stack spacing={2} direction="row">
+          <Button className={styles.button} variant="contained" type="submit">로그인 하기</Button>
+        </Stack>
+      </form>
+      <p className={styles.text}><a href="http://localhost:3000/join">회원이 아니신가요?</a></p>
     </div>
   );
 }
